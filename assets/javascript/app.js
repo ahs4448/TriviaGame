a95fb6a3c1bf3cb8c483821b4ff052bfff0380d7
Jeff Mensing <Jeff Mensing>
2017-02-28 15:18:28 -0600

    correcting bugs and refactoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,20 +14,39 @@ var correctNum = 0;
 var incorrectNum = 0;
 var notAnsweredNum = 0;
 var counter = 0;
+var time = 0;
 var timerCounter = 30;
 
 $(document).ready(function() {
 
     $("#startGame").on("click", function(event) {
         $("#startGame").hide();
-        $("#time").html("<h2>00.00</h2>");
-        $("#trivia").html("<h2>QUESTIONS GO HERE</h2>");
-        generateHTML();
+        //        $("#counter").html("<h2> </h2>");
+        //        $("#trivia").html("<h2>QUESTIONS GO HERE</h2>");
         clock();
+        generateHTML();
+
+    });
+
+    $("body").on("click", ".answer", function(event) {
+        //answeredQuestion = true;
+        selectedAnswer = $(this).text();
+        //con.log for error checking
+        console.log("Selected " + selectedAnswer);
+        console.log(correctArray[counter]);
+        if (selectedAnswer === correctArray[counter]) {
+            alert("correct");
+            clearInterval(time);
+            correctAnswer();
+        } else {
+            alert("wrong answer!");
+            clearInterval(time);
+            //wrongAnswer();
+        }
     });
 
     function clock() {
-        var time = setInterval(thirty, 1000);
+        time = setInterval(thirty, 1000);
 
         function thirty() {
             if (timerCounter === 0) {
@@ -37,33 +56,43 @@ $(document).ready(function() {
             if (timerCounter > 0) {
                 timerCounter--;
             }
-            $("#time").html(timerCounter);
+            $(".timer").html(timerCounter);
         }
     };
 
     function generateHTML() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionsArray[counter] + "</p><p class='first-answer answer'>A. " + answersArray[counter][0] + "</p><p class='answer'>B. " + answersArray[counter][1] + "</p><p class='answer'>C. " + answersArray[counter][2] + "</p><p class='answer'>D. " + answersArray[counter][3] + "</p>";
-        $("#trivia").html(gameHTML);
+        //<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p>
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "<p class='text-center'>" + questionsArray[counter] +
+            "</p><p class='first-answer answer'>A. " + answersArray[counter][0] +
+            "</p><p class='answer'>B. " + answersArray[counter][1] + "</p><p class='answer'>C. " +
+            answersArray[counter][2] + "</p><p class='answer'>D. " + answersArray[counter][3] + "</p>";
+        $(".gameDiv").html(gameHTML);
     };
 
-    $("body").on("click", ".answer", function(event) {
-        //answeredQuestion = true;
-        selectedAnswer = $(this).text();
-        console.log(selectedAnswer);
-        console.log(correctArray[counter]);
-        if (selectedAnswer === correctArray[counter]) {
-            alert("correct");
 
-            //clearInterval(theClock);
-            //generateWin();
+
+    function correctAnswer() {
+        correctNum++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " +
+            answersArray[counter] + "</p>";
+        $(".gameDiv").html(gameHTML);
+        setTimeout(wait, 3000);
+    }
+
+
+
+    function wait() {
+        if (counter < 1) {
+            counter++;
+            generateHTML();
+            timerCounter = 30;
+            clock();
         } else {
-            alert("wrong answer!");
-            //clearInterval(theClock);
-            //generateLoss();
+            finalScreen();
         }
-    });
-
-
+    }
 
 
 
