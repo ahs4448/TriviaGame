1f3b094e664ba2fe3b7d71fb25c5ed303c061cda
Jeff Mensing <Jeff Mensing>
2017-02-28 20:43:07 -0600

    refactoring and fixing bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,6 @@ $(document).ready(function() {
     });
 
     $("body").on("click", ".answer", function(event) {
-        //answeredQuestion = true;
         selectedAnswer = $(this).text();
         //con.log for error checking
         console.log("Selected " + selectedAnswer);
@@ -41,8 +40,12 @@ $(document).ready(function() {
         } else {
             alert("wrong answer!");
             clearInterval(time);
-            //wrongAnswer();
+            wrongAnswer();
         }
+    });
+
+    $(".reset-button").on("click", function(event) {
+        resetGame();
     });
 
     function clock() {
@@ -51,50 +54,83 @@ $(document).ready(function() {
         function thirty() {
             if (timerCounter === 0) {
                 clearInterval(time);
-                lossTimout();
+                questionTimout();
             }
             if (timerCounter > 0) {
                 timerCounter--;
             }
             $(".timer").html(timerCounter);
         }
-    };
+    }
 
     function generateHTML() {
         //<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p>
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
-            timerCounter + "<p class='text-center'>" + questionsArray[counter] +
-            "</p><p class='first-answer answer'>A. " + answersArray[counter][0] +
-            "</p><p class='answer'>B. " + answersArray[counter][1] + "</p><p class='answer'>C. " +
-            answersArray[counter][2] + "</p><p class='answer'>D. " + answersArray[counter][3] + "</p>";
+        gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "</span></h2><h3 class='text-center'>" + questionsArray[counter] +
+            "</h3><h3 class='answer'>A. " + answersArray[counter][0] +
+            "</h3><h3 class='answer'>B. " + answersArray[counter][1] + "</h3><h3 class='answer'>C. " +
+            answersArray[counter][2] + "</h3><h3 class='answer'>D. " + answersArray[counter][3] + "</h3>";
         $(".gameDiv").html(gameHTML);
     };
 
-
-
     function correctAnswer() {
         correctNum++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +
-            timerCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " +
-            answersArray[counter] + "</p>";
+        gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "</span></h2><h3 class='text-center'>Correct! The answer is: " +
+            correctArray[counter] + "</h3>";
         $(".gameDiv").html(gameHTML);
         setTimeout(wait, 3000);
     }
 
+    function wrongAnswer() {
+        incorrectNum++;
+        gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "</span></h2><h3 class='text-center'>Incorrect! The answer is: " +
+            correctArray[counter] + "</h3>";
+        $(".gameDiv").html(gameHTML);
+        setTimeout(wait, 3000);
+    }
 
+    function questionTimeout() {
+        notAnsweredNum++;
+        gameHTML = "h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            timerCounter + "</span></h2><h3 class='text-center'>You ran out of time!</h3>" +
+            "<h3>The correct answer is: "
+        correctArray[counter] + "</h3>";
+        $(".gameDiv").html(gameHTML);
+        setTimeout(wait, 3000);
+    }
+
+    function endOfGame() {
+        gameHTML = "<h2 class='text-center timer-p'>Time Remaining: <span class='timer'>" +
+            counter + "</span></h2><h3 class='text-center'>All done, here's how you did!</h3>" +
+            "<h3 class='summary-correct'>Correct Answers: " + correctNum +
+            "</h3><h3>Wrong Answers: " + incorrectNum + "</h3><h3>Unanswered: " +
+            notAnsweredNum + "</h3>" +
+            "<h3 class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></h3>";
+        $(".gameDiv").html(gameHTML);
+    }
 
     function wait() {
         if (counter < 1) {
             counter++;
+            console.log(counter);
             generateHTML();
             timerCounter = 30;
             clock();
         } else {
-            finalScreen();
+            endOfGame();
         }
     }
 
-
+    function resetGame() {
+        var correctNum = 0;
+        var incorrectNum = 0;
+        var notAnsweredNum = 0;
+        var counter = 0;
+        var time = 0;
+        var timerCounter = 30;
+    }
 
 
 });
